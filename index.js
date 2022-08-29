6778df0a2624fdb7545a37e36b96edade2727d91
RK03ind <gamitech95@gmail.com>
2022-08-29 23:03:05 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-document.querySelector(".translate").addEventListener("click", () => {
+document.querySelector(".translate-button").addEventListener("click", () => {
   var text = document.querySelector(".text-input").value;
+  if (!text) return alert("Enter Something to translate");
+  document.querySelector(".translate").innerText = "Translating...";
   fetch(`https://api.funtranslations.com/translate/minion.json?text=${text}`)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      document.querySelector(".text-output").innerHTML =
+      document.querySelector(".translate").innerText = "Translate";
+      document.querySelector(".text-output").innerText =
         data.contents.translated;
+    })
+    .catch((error) => {
+      alert("Something went wrong");
     });
 });
