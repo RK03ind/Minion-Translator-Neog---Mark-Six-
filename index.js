440ed2d0d465e222979f6c3eeffc5f369b492490
RK03ind <gamitech95@gmail.com>
2022-08-30 02:53:35 +0530

    Fix: Button Selector

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.querySelector(".translate-button").addEventListener("click", () => {
   var text = document.querySelector(".text-input").value;
   if (!text) return alert("Enter Something to translate");
-  document.querySelector(".translate").innerText = "Translating...";
+  document.querySelector(".translate-button").innerText = "Translating...";
   fetch(`https://api.funtranslations.com/translate/minion.json?text=${text}`)
     .then((response) => {
       return response.json();
@@ -12,6 +12,7 @@ document.querySelector(".translate-button").addEventListener("click", () => {
         data.contents.translated;
     })
     .catch((error) => {
+      document.querySelector(".translate-button").innerText = "Translate";
       alert("Something went wrong");
     });
 });
